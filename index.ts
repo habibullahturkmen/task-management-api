17f7f667b64f73afae2b0fcdcb5c2b54c469a598
habibullahturkmen <habibullah_turkmen@yahoo.com>
2023-04-01 02:05:39 +0430

    typeorm added and connected to postgres

--- index.ts
+++ index.ts
@@ -1,9 +1,21 @@
 import express, { Express, Request, Response} from "express"
 import dotenv from "dotenv"
+import { DataSource } from "typeorm"
 
 // instantiate express app
 const app: Express = express()
 dotenv.config()
+
+// create database connection
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  synchronize: process.env.NODE_ENV !== "production"
+})
 
 // define server port
 const port = process.env.PORT
@@ -13,5 +25,10 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server Running")
 })
 
-// listen to the port
-app.listen(port)
+// initialize data source and listen to the port
+AppDataSource.initialize().then(() => {
+  app.listen(port)
+  console.log("Data Source has been initialized!")
+}).catch((err) => {
+  console.error("Error during Data Source initialization", err)
+})
